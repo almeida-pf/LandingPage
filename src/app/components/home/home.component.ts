ccba84fcf2f2f6034ad7b51563630efa1051746f
Pablo Almeida <31329742+almeida-pf@users.noreply.github.com>
2024-01-23 17:09:30 -0300

    Update home component HTML, SCSS, and imports

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { HeaderComponent } from './../header/header.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NgOptimizedImage],
   providers: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
